437cb3bc112f7a7aed3554470187e88e0862b5b3
Hong Baojin <13809735212@163.com>
2019-10-08 14:54:42 +0800

    first commit

---
+++ src/components/Count.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2><hr/>
+    <h3>{{$store.state.a.count}} - {{count}}</h3>
+    <p>
+     <button @click="$store.commit('add', 10)">+</button>
+     <button @click="reduce">-</button>
+    </p>
+    <p>
+     <button @click="addAction">+</button>
+     <button @click="reduceAction">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  import store from '@/vuex/store'
+  import { mapState, mapMutations, mapGetters, mapActions } from 'vuex';
+  export default {
+    data(){
+      return{
+        msg: 'Hello Vuex'
+      }
+    },
+    // 第一种方法
+    // computed: {
+    //   count(){
+    //     return this.$store.state.count;
+    //   }
+    // },
+    // 第二种方法
+    // computed: mapState({
+    //   count: state => state.count
+    // }),
+    // 第三种方法
+    // computed: mapState(['count']),
+
+    computed: {
+      ...mapState(['count']),
+      // count(){
+      //   return this.$store.getters.count;
+      // }
+      // ...mapGetters(['count'])
+
+      count(){
+        return this.$store.state.a.count;
+      }
+    },
+
+    methods: {
+      ...mapMutations(['add', 'reduce']),
+      ...mapActions(['addAction', 'reduceAction'])
+    },
+    store
+  }
+</script>
